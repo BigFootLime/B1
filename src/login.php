cbce2069571b8756f4649376a35b8a143283fbb1
Echo-Toki <155429877+Echo-Toki@users.noreply.github.com>
2024-06-17 13:40:28 +0200

    Modif Login Access dash

--- src/login.php
+++ src/login.php
@@ -24,7 +24,7 @@
         </form>
     </div>
     <div class="form-container sign-in-container">
-        <form action="action='dashboard.php' method="POST">
+        <form action='dashboard.php' method="POST">
             <h1>Sign in</h1>
             <div class="social-container">
                 <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
