40e9a3544550b3762421237875b8c89ab624b685
Keenan MARTIN <kesmartin2004@gmail.com>
2024-06-20 23:55:29 +0200

    Remove overflow hidden from code for responsive

--- src/accueil.php
+++ src/accueil.php
@@ -18,7 +18,7 @@
         }
     </style>
  <!-- **************************************************SIDEBAR*********************************************************************** -->
-<body class="bg-gradient-to-r from-[#04CD59] to-[#05B6A1] overflow-hidden font-poppins">
+<body class="bg-gradient-to-r from-[#04CD59] to-[#05B6A1] font-poppins">
 <div class="text-center py-2 font-poppins">
         <button id="drawer-button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
             Show navigation
