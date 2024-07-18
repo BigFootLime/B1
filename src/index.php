0df4e437ae84055b4da304008689459e20ca0dbe
Keenan MARTIN <kesmartin2004@gmail.com>
2024-07-18 09:26:46 +0200

    FIX ERROR COLOR LOGIN

--- src/index.php
+++ src/index.php
@@ -3,11 +3,13 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./output.css">
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
   <title>Document</title>
 </head>
-<body class='font-poppins'>
+<body class='bg-gray-900 h-screen overflow-hidden font-poppins'>
 <?php
 include_once 'login.php';
 ?>
